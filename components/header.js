c2a279618829ffe98e46cf631fd9a7ea89833cfa
croixxant <croixxxant@gmail.com>
2021-02-23 08:20:37 +0900

    implemented transition

--- components/header.js
+++ components/header.js
@@ -1,7 +1,10 @@
-import * as React from 'react'
+import React, { useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { css } from '@emotion/css'
 import tw from 'twin.macro'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div tw="relative bg-white">
       <div tw="max-w-7xl mx-auto px-4 sm:px-6">
@@ -15,7 +18,8 @@ const Header = () => {
             <button
               type="button"
               tw="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-              aria-expanded="false"
+              aria-expanded={isOpen}
+              onClick={() => setIsOpen(!isOpen)}
             >
               <span tw="sr-only">Open menu</span>
               <svg tw="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -48,61 +52,72 @@ const Header = () => {
         </div>
       </div>
 
-      <div tw="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-        <div tw="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-          <div tw="pt-5 pb-6 px-5">
-            <div tw="flex items-center justify-between">
-              <div tw="flex justify-start lg:w-0 lg:flex-1">
-                <a href="/">
-                  <span tw="">Croixxant.me</span>
+      <Transition
+        show={isOpen}
+        enter={css(tw`transition duration-200 ease-out`)}
+        enterFrom={css(tw`opacity-0 scale-95`)}
+        enterTo={css(tw`opacity-100 scale-100`)}
+        leave={css(tw`transition duration-100 ease-in`)}
+        leaveFrom={css(tw`opacity-100 scale-100`)}
+        leaveTo={css(tw`opacity-0 scale-95`)}
+      >
+        <div tw="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+          <div tw="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
+            <div tw="pt-5 pb-6 px-5">
+              <div tw="flex items-center justify-between">
+                <div tw="flex justify-start lg:w-0 lg:flex-1">
+                  <a href="/">
+                    <span tw="">Croixxant.me</span>
+                  </a>
+                </div>
+                <div tw="-mr-2">
+                  <button
+                    type="button"
+                    tw="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                    onClick={() => setIsOpen(!isOpen)}
+                  >
+                    <span tw="sr-only">Close menu</span>
+                    <svg
+                      tw="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div tw="py-4 px-5 space-y-6">
+              <div>
+                <a href="#" tw="w-full flex px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700">
+                  Articles
+                </a>
+                <a href="#" tw="w-full flex px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700">
+                  Scraps
                 </a>
               </div>
-              <div tw="-mr-2">
-                <button
-                  type="button"
-                  tw="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              <div tw="grid grid-cols-2 gap-y-4 gap-x-8">
+                <a
+                  href="#"
+                  tw="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
-                  <span tw="sr-only">Close menu</span>
-                  <svg
-                    tw="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
+                  Twitter
+                </a>
+                <a
+                  href="#"
+                  tw="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                >
+                  Github
+                </a>
               </div>
-            </div>
-          </div>
-          <div tw="py-4 px-5 space-y-6">
-            <div>
-              <a href="#" tw="w-full flex px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700">
-                Articles
-              </a>
-              <a href="#" tw="w-full flex px-4 py-2 text-base font-medium text-gray-900 hover:text-gray-700">
-                Scraps
-              </a>
-            </div>
-            <div tw="grid grid-cols-2 gap-y-4 gap-x-8">
-              <a
-                href="#"
-                tw="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Twitter
-              </a>
-              <a
-                href="#"
-                tw="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Github
-              </a>
             </div>
           </div>
         </div>
-      </div>
+      </Transition>
     </div>
   )
 }
